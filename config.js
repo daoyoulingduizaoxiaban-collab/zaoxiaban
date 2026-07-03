cd09addeb92c33fdeadd44c63e85ce02abbbee59
Ivy <daoyoulingduizaoxiaban@gmail.com>
2026-07-03 13:48:41 +0800

    Fix visible icon fallbacks

--- config.js
+++ config.js
@@ -10,24 +10,28 @@ export const BOTTOM_BAR_LIST = [
   // { icon: 'home', value: 'home', label: '首頁', path: '/pages/home/index' },
   {
     icon: 'bulletpoint',
+    symbol: '单',
     value: 'groupOrder',
     label: '团单',
     path: '/pages/groupOrder/index'
   },
   {
     icon: 'usergroup',
+    symbol: '客',
     value: 'customerOrders',
     label: '客户订单',
     path: '/pages/customerOrders/index'
   },
   {
     icon: 'data-display',
+    symbol: '品',
     value: 'productManagement',
     label: '商品库',
     path: '/pages/productManagement/index'
   },
   {
     icon: 'user',
+    symbol: '我',
     value: 'my',
     label: '我的',
     path: '/pages/my/index'
